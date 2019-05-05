47118d027de0644c1310b99955c740ea895effca
Marcin Wiatr <marcin@qi-interactive.com>
2019-05-05 11:55:09 +0100

    Added caching

--- Module.php
+++ Module.php
@@ -8,55 +8,202 @@
 
 namespace matacms\environment;
 
-use mata\base\Module as BaseModule;
+use Yii;
+use yii\base\Event;
+use matacms\base\MessageEvent;
+use mata\arhistory\behaviors\HistoryBehavior;
+use mata\helpers\BehaviorHelper;
+use matacms\controllers\module\Controller;
+use matacms\environment\models\ItemEnvironment;
+use matacms\environment\Module;
+use yii\db\BaseActiveRecord;
+use mata\db\ActiveQuery;
+use yii\web\HttpException;
+use matacms\user\models\User;
+use yii\helpers\Url;
 
-class Module extends BaseModule {
+class Bootstrap extends \mata\base\Bootstrap {
 
-	/**
-	 *  Name of the live environment
-	 */ 
-	public $liveEnvironment;
+	private static $envQueryCount = -1;
 
-	/**
-	 *  Name of the stage environment, which is not accessible by users who are not logged in.
-	 */ 
-	public $stageEnvironment;
+	public function bootstrap($app) {
 
-	const DEFAULT_LIVE_ENVIRONMENT = "LIVE";
-	const DEFAULT_STAGE_ENVIRONMENT = "DRAFT";
-	const DEFAULT_SUPERSEDED_ENVIRONMENT = "SUPERSEDED";
-	
-	public function init() {
+		Event::on(BaseActiveRecord::class, BaseActiveRecord::EVENT_AFTER_INSERT, function(Event $event) {
+			$this->processSave($event->sender);
+		});
 
-		parent::init();
+		Event::on(BaseActiveRecord::class, BaseActiveRecord::EVENT_AFTER_UPDATE, function(Event $event) {
+			$this->processSave($event->sender);
+		});
 
-		if ($this->liveEnvironment == null)
-			$this->liveEnvironment = self::DEFAULT_LIVE_ENVIRONMENT;
+		Event::on(ActiveQuery::class, ActiveQuery::EVENT_BEFORE_PREPARE_STATEMENT, function(Event $event) {
+
+			// When logged into the CMS, latest version should be shown
+			if (!is_a(\Yii::$app, "yii\console\Application") && \Yii::$app->user->isGuest ) {
+
+				$activeQuery = $event->sender;
+				$modelClass = $activeQuery->modelClass;
+				$sampleModelObject = new $modelClass;
+
+				if (BehaviorHelper::hasBehavior($sampleModelObject, \matacms\environment\behaviors\EnvironmentBehavior::class)) {
+
+					$documentIdBase = $sampleModelObject->getDocumentId()->getId();
+					$tableAlias = $activeQuery->getQueryTableName($activeQuery)[0];
+
+					if (count($modelClass::primaryKey()) > 1) {
+						throw new HttpException(500, sprintf("Composite keys are not handled yet. Table alias is %s", $tableAlias));
+					}
+
+					$tablePrimaryKey = $modelClass::primaryKey()[0];
+
+					// if ($activeQuery->join)
+					// 	foreach ($activeQuery->join as $join) {
+					// 		$tableToJoin = $join[1];
+					// 	 	$this->addItemEnvironmentJoin($activeQuery, $tableToJoin  . ".DocumentId", $documentIdBase);
+					// 	}
+
+					$this->addItemEnvironmentJoin($activeQuery, "CONCAT('" . $documentIdBase . "', " . $tableAlias . "." . $tablePrimaryKey . ")", $documentIdBase);
+
+				}
+
+
+			}
+		});
+
+		Event::on(BaseActiveRecord::class, BaseActiveRecord::EVENT_AFTER_FIND, function(Event $event) {
+
+			if (!is_a(\Yii::$app, "yii\console\Application") && Yii::$app->getRequest()->get(ItemEnvironment::REQ_PARAM_REVISION)) {
+				$model = $event->sender;
+				$this->getRevision($model, Yii::$app->getRequest()->get(ItemEnvironment::REQ_PARAM_REVISION));
+			}
+
+		});
+
 	}
 
-	public function getNavigation() {
-		return false;
+	private function addItemEnvironmentJoin($activeQuery, $documentId, $documentIdBase) {
+
+		$module = \Yii::$app->getModule("environment");
+
+		if ($module == null)
+			throw new \yii\base\InvalidConfigException("'environment' module pointing to matacms\\environment\\Module module needs to be set");
+
+		/**
+		 * We need to check if a given [[documentId]] is present in the [[ItemEnvironment]] table.
+		 * If is it not, it means that environments are not used for a given [[documentId]]
+		 * This check cannot be done with [[BehaviorHelper::hasBehavior]], as we not always have
+		 * the model class name, but always have the [[documentId]]
+		 */
+
+        $hasEnvironmentRecords = \Yii::$app->cache->get(__CLASS__ . "_has_records_" . md5($documentIdBase));
+
+        if ($hasEnvironmentRecords === false) {
+            $hasEnvironmentRecords = ItemEnvironment::find()->where(['like', 'DocumentId', $documentIdBase])->limit(1)->one();
+            \Yii::$app->cache->set(__CLASS__ . "_has_records_" . md5($documentIdBase), $hasEnvironmentRecords);
+        }
+
+		if ($hasEnvironmentRecords == null)
+			return;
+
+		$alias = $this->getTableAlias();
+
+		// TODO This encoding happens in Yii, use what they're offering. E.g. it is used in the call on line 91
+		$documentId = str_replace("\\", "\\\\",  $documentId);
+
+		if ($activeQuery->select == null)
+			$activeQuery->addSelect(["*", $alias . ".Revision"]);
+		else
+			$activeQuery->addSelect($alias . ".Revision");
+
+		$activeQuery->innerJoin("matacms_itemenvironment " . $alias, "`" . $alias . "`.`DocumentId` = " . $documentId);
+
+		 // TODO refactor and use Query!
+		 if (Yii::$app->user->isGuest) {
+		 	$liveEnvironment = $module->getLiveEnvironment();
+
+		 	$activeQuery->andWhere($alias . ".Revision = (SELECT Revision FROM matacms_itemenvironment " . $alias . "rev WHERE . " . $alias . "rev.`DocumentId` = " . $alias . ".DocumentId
+		 	 			 AND " . $alias . "rev.`Status` = '" . $liveEnvironment . "' ORDER BY " . $alias . ".Revision DESC LIMIT 1)");
+		 	} else {
+		 		$activeQuery->andWhere($alias . ".Revision = (SELECT Revision FROM matacms_itemenvironment " . $alias . "rev WHERE " . $alias . "rev.`DocumentId` = " . $alias . ".DocumentId
+		 			 			 ORDER BY " . $alias . ".Revision DESC LIMIT 1)");
+
+		 	}
 	}
 
-	public function getLiveEnvironment() {
-		return $this->liveEnvironment ?: Module::DEFAULT_LIVE_ENVIRONMENT;
+	private function getTableAlias() {
+		return "env" . ++self::$envQueryCount;
 	}
 
-	public function getStageEnvironment() {
-		return $this->stageEnvironment ?: Module::DEFAULT_STAGE_ENVIRONMENT;		
+	private function shouldRun() {
+		return Yii::$app->user->isGuest;
 	}
 
-	public function getSupersededEnvironment() {
-		return Module::DEFAULT_SUPERSEDED_ENVIRONMENT;		
+	private function getRevision($model, $revision) {
+		if (BehaviorHelper::hasBehavior($model, \mata\arhistory\behaviors\HistoryBehavior::class))
+			$model->setRevision($revision);
 	}
 
-	public function hasEnvironmentBehavior($model) {
-		foreach ($model->getBehaviors() as $behavior) {
-			if (is_a($behavior, \matacms\environment\behaviors\EnvironmentBehavior::class))
-				return true;
+	private function processSave($model) {
+
+		if (is_object($model) == false ||
+			BehaviorHelper::hasBehavior($model, \mata\arhistory\behaviors\HistoryBehavior::class) == false)
+			return;
+
+		$module = \Yii::$app->getModule("environment");
+		$liveEnvironment = $module->getLiveEnvironment();
+
+		/**
+		 * Some core models don't have versions, but use media which have versions.
+		 * Assume in such case that we are publishing straight away
+		 */
+		$status = Yii::$app->getRequest()->post(ItemEnvironment::REQ_PARAM_ITEM_ENVIRONMENT, $liveEnvironment);
+
+		$reviewerId = Yii::$app->getRequest()->post('reviewer', null);
+
+		$supersededEnvironment = $module->getSupersededEnvironment();
+
+		if ($status == $liveEnvironment) {
+			ItemEnvironment::updateAll(['Status' => $supersededEnvironment], 'DocumentId = :documentId AND Status = :status', [':documentId' => $model->getDocumentId()->getId(), ':status' => $liveEnvironment]);
 		}
 
-		return false;
+		$ie = new ItemEnvironment();
+		$ie->attributes = [
+			"DocumentId" => $model->getDocumentId()->getId(),
+			"Revision" => $model->getLatestRevision()->Revision,
+			"Status" => $status
+		];
+		if (!$ie->save())
+			throw new \yii\web\ServerErrorHttpException($ie->getTopError());
+
+		if(!empty($reviewerId)) {
+			$reviewerModel = User::find()->where(['id' => $reviewerId])->asArray()->one();
+			if(!empty($reviewerModel)) {
+				$updateRoute = '/'.Yii::$app->controller->module->id . '/' . Yii::$app->controller->id . '/update';
+
+				$documentForReviewUrl = Url::to([$updateRoute, 'id' => $model->getDocumentId()->getPk(), 'revision' => $ie->Revision], true);
+				$this->sendReviewMessage($reviewerModel['email'], [
+					'authorName' => Yii::$app->user->identity->username,
+					'documentForReviewUrl' => $documentForReviewUrl,
+					'modelLabel' => $model->getModelLabel(),
+					'label' => $model->getLabel()
+					]);
+
+				Yii::$app->getSession()->addFlash(MessageEvent::LEVEL_SUCCESS, 'Request for review has been sent to ' . Yii::$app->user->identity->username);
+			}
+		}
 	}
 
+	private function sendReviewMessage($to, $params)
+	{
+		$mailer = \Yii::$app->mailer;
+		$sender = \Yii::$app->getModule('user')->mailer['sender'];
+        $mailer->viewPath = '@matacms/rbac/views/mail';
+        $mailer->getView()->theme = \Yii::$app->view->theme;
+
+        return $mailer->compose(['html' => 'review', 'text' => 'text/review'], $params)
+            ->setTo($to)
+            ->setFrom($sender)
+            ->setSubject('Review request')
+            ->send();
+	}
 }
